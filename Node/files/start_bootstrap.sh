4503440c94f820227141e9a39b99f33744b5a6f0
jcremerius <jonas.cremerius@student.hpi.de>
2018-01-24 14:52:47 +0100

    Modify the startup files, so the XAIN blockchain is up and running

--- Node/files/start_bootstrap.sh
+++ Node/files/start_bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /root/files
-/root/files/geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
-./geth --datadir=~/data --password <(echo -n 123)  account new
+./geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
 ./geth $GETH_OPTS
--- Node/files/start_eth_node.sh
+++ Node/files/start_eth_node.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /root/files
 ./geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
-./geth --datadir=~/data --password <(echo -n 123)  account new
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
 python3 /root/files/node.py &
