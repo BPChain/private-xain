34383c5c1eaf58bd635fd7a1b3994e4a1cdaf1af
jcremerius <jonas.cremerius@student.hpi.de>
2018-01-30 11:05:08 +0100

    Add individual coinbasepwd files, so the nodes can start mining

--- Node/files/start_eth_node.sh
+++ Node/files/start_eth_node.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cp /root/keys/coinbasepwd_$XAIN_NUMBER /root/data
+mv /root/data/coinbasepwd_$XAIN_NUMBER /root/data/coinbasepwd
+cp /root/keys/XAIN_key_$XAIN_NUMBER /root/data/keystore
 cd /root/files
 ./geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
