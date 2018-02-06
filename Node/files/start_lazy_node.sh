4e99e36b5ef117d92e5ed707d8ad01d07d4a8ddc
jcremerius <jonas.cremerius@student.hpi.de>
2018-02-06 14:02:40 +0100

    Replace the IP of the api server

--- Node/files/start_lazy_node.sh
+++ Node/files/start_lazy_node.sh
@@ -4,5 +4,4 @@ cd /root/files || exit
 ./geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
-python3 /root/files/node.py &
 ./geth $GETH_OPTS
