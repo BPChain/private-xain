a1a0461f78a2d97b13a04d9a27e224470ee396a3
jcremerius <jonas.cremerius@student.hpi.de>
2018-03-29 14:01:41 +0200

    Add EVAPCoin to XAIN chain

---
+++ Node/files/EVAPcoin/scripts/slave/startSimulation.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+const iterationTime = process.argv[2]
+
+function start() {
+  var ws
+    ws = new WebSocket('ws://eth_contract_deployer:40000')
+     ws.on('message', function incoming(address) {
+          console.log("-------------------------Addresse-------------")
+          console.log(address)
+          require("./simulateContract")(address, iterationTime).then(function () {
+         })
+     })
+  ws.onerror=function(event) {
+
+      setTimeout(function () {
+          console.log("Error");
+          start()
+        }, 10000)
+  }
+
+}
+start()
+
