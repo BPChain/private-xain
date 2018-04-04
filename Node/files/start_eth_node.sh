ad4ec47a8578935061306176c64b9e4116132f99
jcremerius <jonas.cremerius@student.hpi.de>
2018-04-04 11:35:02 +0200

    rename container so that they are xain specific

--- Node/files/start_eth_node.sh
+++ Node/files/start_eth_node.sh
@@ -8,7 +8,7 @@ cp /root/data/coinbasepwd /root/files
 cp /root/keys/XAIN_key_$XAIN_NUMBER /root/data/keystore
 cd /root/files || exit
 ./geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
-BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
-GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
+XAIN_BOOTSTRAP_IP=`getent hosts xain_bootstrap | cut -d" " -f1`
+GETH_OPTS=${@/IPAddress/$XAIN_BOOTSTRAP_IP}
 python3 /root/files/node.py &
 ./geth $GETH_OPTS
--- Node/files/start_lazy_node.sh
+++ Node/files/start_lazy_node.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 cd /root/files || exit
 ./geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
-BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
-GETH_OPTS=${@/IPAddress/$BOOTSTRAP_IP}
+XAIN_BOOTSTRAP_IP=`getent hosts xain_bootstrap | cut -d" " -f1`
+GETH_OPTS=${@/IPAddress/$XAIN_BOOTSTRAP_IP}
 ./geth $GETH_OPTS
