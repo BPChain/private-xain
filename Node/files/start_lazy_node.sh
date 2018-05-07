c533a1640cc6850772507cf54f1ff6028e3ef5be
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-07 14:01:05 +0200

    Add the MetaScenario to the project

--- Node/files/start_lazy_node.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-cd /root/files || exit
-./geth --datadir=~/data init "/root/files/blockchain_files/genesis.json"
-XAIN_BOOTSTRAP_IP=`getent hosts xain_bootstrap | cut -d" " -f1`
-GETH_OPTS=${@/IPAddress/$XAIN_BOOTSTRAP_IP}
-./geth $GETH_OPTS
