2412f17c7034dbc49791d9366e60b83128e52f64
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-07 16:19:11 +0200

    Add interval variable

--- Node/files/METAScenario/migrations/1_initial_migration.js
+++ Node/files/METAScenario/migrations/1_initial_migration.js
@@ -3,11 +3,11 @@ const Web3 = require('web3')
 
 module.exports = function(deployer) {
   var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
-  setInterval(function() {
+  var interval = setInterval(function() {
         try {
             provider.eth.defaultAccount = provider.eth.accounts[0];
           if(provider.eth.getBalance(provider.eth.accounts[0]) > 100000) {
-              clearInterval();
+              clearInterval(interval);
               provider.eth.defaultAccount = provider.eth.accounts[0];
               provider.personal.unlockAccount(provider.eth.accounts[0], "1234567890");
               deployer.deploy(Migrations);
