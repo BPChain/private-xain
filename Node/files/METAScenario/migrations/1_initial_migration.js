64de8f66a09d830461923ee34d67e6136f4364c9
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-07 15:12:11 +0200

    Add simulation on node

--- Node/files/METAScenario/migrations/1_initial_migration.js
+++ Node/files/METAScenario/migrations/1_initial_migration.js
@@ -5,13 +5,11 @@ module.exports = function(deployer) {
   var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8547"));
   setInterval(function() {
         try {
-          if(provider.eth.getBalance(eth.accounts[0]) > 100000) {
-              console.log("%%%%%%%%%%%%%%%%%%%%%%%%%Before clear%%%%%%%%%%%%%%%%%")
+          if(provider.eth.getBalance(provider.eth.accounts[0]) > 100000) {
               clearInterval();
-              console.log("%%%%%%%%%%%%%%%%%%%%%%%%%After clear%%%%%%%%%%%%%%%%")
               provider.miner.stop();
               provider.eth.defaultAccount = provider.eth.accounts[0];
-              provider.personal.unlockAccount(eth.accounts[0], "1234567890");
+              provider.personal.unlockAccount(provider.eth.accounts[0], "1234567890");
               deployer.deploy(Migrations);
           }
 
