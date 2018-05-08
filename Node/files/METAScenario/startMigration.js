9e4f872ad0779dd9bc3c3c8908f71b3737b8fbf3
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-08 11:14:17 +0200

    Modify the migration, so the contract is deployed via web 3

--- Node/files/METAScenario/startMigration.js
+++ Node/files/METAScenario/startMigration.js
@@ -1,19 +1,11 @@
 const execa = require("execa")
 var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
 const Web3 = require('web3')
-
+var web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 function start() {
-        execa('truffle', ['migrate', '--network=dev']).then(function (result) {
-        console.log(result)
-        process.exit(0)
-    }).catch(function (error){
-        setTimeout(function () {
-            console.log("Error while migrating")
-            console.log(error)
-            start()
-        }, 15000)
+    var simplecoinContract = web3.eth.contract(abi);
 
-    })
+
 
 }
 
