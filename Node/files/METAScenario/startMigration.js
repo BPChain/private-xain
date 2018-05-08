49b1809329add1face683fa6ec3e51ad9683bc55
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-08 13:23:23 +0200

    Let the miner stop before deploying the contract

--- Node/files/METAScenario/startMigration.js
+++ Node/files/METAScenario/startMigration.js
@@ -6,6 +6,9 @@ function start() {
     try {
         console.log("#################################")
         var account = web3.eth.accounts[0];
+        if(web3.eth.getBalance(account) > 100000) {
+            console.log("Stopping mining")
+            web3.miner.stop();
         var metascenarioContract = web3.eth.contract([{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"}]);
         var metascenario = metascenarioContract.new(
             {
@@ -25,7 +28,7 @@ function start() {
                     }, 10000)
                 }
  })
-
+    }
 
     } catch(error) {
         console.log("%%%%%%%%%%%%%%%%Failed%%%%%%%%%%%%%%%%")
