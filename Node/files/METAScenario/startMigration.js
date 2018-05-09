3e0aa9831a35e2138651e1f7d3e2956a82470775
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-09 09:29:03 +0200

    Call the migration script again if the contract is undefined

--- Node/files/METAScenario/startMigration.js
+++ Node/files/METAScenario/startMigration.js
@@ -1,6 +1,8 @@
 const execa = require("execa")
 var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
 const Web3 = require('web3')
+const ws = require('ws')
+const WebSocketServer = ws.Server
 const web3Admin = require('web3admin')
 var web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 setTimeout(function () {
@@ -25,15 +27,21 @@ function start() {
                 web3.miner.start();
                 if (!e) {
                 }
-
-                if (!contract.address) {
-                    console.log("no address yet")
-                } else {
-                    console.log(contract.address)
-                    const wsServer = new WebSocketServer({port: 40000})
-                    wsServer.on('connection', function (connection) {
-                        connection.send(contract.address)
-                    })
+                try {
+                    if (!contract.address) {
+                        console.log("no address yet")
+                    } else {
+                        console.log(contract.address)
+                        const wsServer = new WebSocketServer({port: 40000})
+                        wsServer.on('connection', function (connection) {
+                            connection.send(contract.address)
+                        })
+                    }
+                } catch (error) {
+                    console.log(error)
+                    setTimeout(function () {
+                        start()
+                    }, 10000)
                 }
             })
         } else {
