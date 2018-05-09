3087cfdf935bc79de2588a61b00296e483b40a03
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-09 13:20:42 +0200

    Add debug info

--- Node/files/METAScenario/scripts/slave/startSimulation.js
+++ Node/files/METAScenario/scripts/slave/startSimulation.js
@@ -7,8 +7,8 @@ const fs = require('fs')
 var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
 var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 setTimeout(function () {
-            web3Admin.extend(provider)
-        }, 1000)
+    web3Admin.extend(provider)
+}, 1000)
 function start() {
   var ws
     ws = new WebSocket('ws://xain_contract_deployer:40000')
@@ -47,10 +47,12 @@ function startSimulate(address, iterationTime) {
 
 function generateCoins(address) {
         try {
+            const account = provider.eth.accounts[0]
             const coinbasepwd = fs.readFileSync("/root/files/coinbasepwd", "utf8")
+            console.log(coinbasepwd)
             var METAScenario = provider.eth.contract(abi).at(address)
-            provider.eth.defaultAccount = provider.eth.accounts[0]
-            provider.personal.unlockAccount(provider.eth.accounts[0], coinbasepwd)
+            provider.eth.defaultAccount = account
+            provider.personal.unlockAccount(account, coinbasepwd)
             provider.miner.stop()
             METAScenario.generate(9999999999999)
             provider.miner.start()
