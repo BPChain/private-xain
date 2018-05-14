0a7f5f9c6eebec4d03a04aa0e3f682ad0d66a8a5
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-14 10:20:32 +0200

    Replace magic numbers

--- Node/files/METAScenario/scripts/startSimulation.js
+++ Node/files/METAScenario/scripts/startSimulation.js
@@ -4,6 +4,7 @@ const execa = require("execa")
 const Web3 = require('web3')
 const web3Admin = require('web3admin')
 const fs = require('fs')
+const requiredBalance = 9999999999
 var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
 var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 web3Admin.extend(provider)
@@ -31,7 +32,7 @@ function start() {
 }
 
 function startSimulate(address, iterationTime) {
-    if (provider.eth.getBalance(provider.eth.accounts[0]) > 9999999999) {
+    if (provider.eth.getBalance(provider.eth.accounts[0]) > requiredBalance) {
         generateCoins(address)
         setTimeout(function () {
             require("./simulateTransactions")(address, iterationTime)
@@ -53,7 +54,7 @@ function generateCoins(address) {
             provider.eth.defaultAccount = account
             provider.miner.stop()
             provider.personal.unlockAccount(account, coinbasepwd)
-            METAScenario.generate(9999999999999)
+            METAScenario.generate(requiredBalance)
             provider.miner.start()
             setTimeout(function () {
                 generateCoins(address)
