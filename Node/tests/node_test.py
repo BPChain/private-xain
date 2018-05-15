f5c27f2a64d3c588a1840c9b81fac64292c081ad
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-15 14:21:56 +0200

    Fix test

--- Node/tests/node_test.py
+++ Node/tests/node_test.py
@@ -5,10 +5,10 @@ from attrdict import AttrDict
 
 from Node.files import node
 
-LAST_SENT_BLOCK = AttrDict({'number': 0, 'difficulty': 2.0, 'timestamp': 1})
-BLOCKS = [AttrDict({'number': 1, 'difficulty': 2.0, 'timestamp': 2}),
-          AttrDict({'number': 2, 'difficulty': 3.0, 'timestamp': 3}),
-          AttrDict({'number': 3, 'difficulty': 4.0, 'timestamp': 4})]
+LAST_SENT_BLOCK = AttrDict({'number': 0, 'difficulty': 2.0, 'timestamp': 1, 'size': 1})
+BLOCKS = [AttrDict({'number': 1, 'difficulty': 2.0, 'timestamp': 2, 'size': 1}),
+          AttrDict({'number': 2, 'difficulty': 3.0, 'timestamp': 3, 'size': 1}),
+          AttrDict({'number': 3, 'difficulty': 4.0, 'timestamp': 4, 'size': 1})]
 
 
 def test_avg_difficulty():
@@ -44,7 +44,7 @@ def test_provide_data_for_first_block():
     web3.eth.getBlock = getBlock
     number_of_last_block = 0
     node_data = {"avgDifficulty": 0, "avgBlocktime": 0}
-    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3)
+    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3, 'host')
     assert last_b_num == 3
     assert node_data['avgDifficulty'] == 0
     assert node_data['avgBlocktime'] == 0
@@ -59,7 +59,7 @@ def test_provide_data():
     web3.eth.getBlock = getBlock
     number_of_last_block = 1
     node_data = {"avgDifficulty": 0, "avgBlocktime": 0}
-    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3)
+    last_b_num, node_data = node.provide_data(number_of_last_block, node_data, web3, 'host')
     assert last_b_num == 3
     assert node_data['avgDifficulty'] == 3.5
     assert node_data['avgBlocktime'] == 1
