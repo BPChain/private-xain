8b199db7ed06d49ccbcece6c9d0d69bbf5d3a82d
jcremerius <jonas.cremerius@student.hpi.de>
2018-05-28 12:24:16 +0200

    Refactor javascript

--- files/METAScenario/scripts/runTransactionSlave.js
+++ files/METAScenario/scripts/runTransactionSlave.js
@@ -4,74 +4,76 @@ const request = require('request')
 const ip = require('ip')
 const web3Admin = require('web3admin')
 const fs = require('fs')
+const abi = require('../contractAbi.json')
+
 
 module.exports = function (address) {
 
-    var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
-    web3Admin.extend(provider)
-    var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
+  const provider = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
+  web3Admin.extend(provider)
+  function initialize () {
+    try {
+      const coinbasepwd = fs.readFileSync('/root/files/coinbasepwd', 'utf8')
+        .replace('\n', '')
+      const METAScenario = provider.eth.contract(abi)
+        .at(address)
+      provider.eth.defaultAccount = provider.eth.accounts[0]
+      const ipAddress = {
+        ipAddress: ip.address()
+          .toString(),
+      }
+      request({
+        url: 'http://xain_contract_deployer:60000',
+        method: 'POST',
+        json: true,
+        body: ipAddress,
+      }, (error) => {
+        if (error) {
+          console.log(error)
+        }
+        console.log('connected')
+      })
+      startws(METAScenario, coinbasepwd)
+    }
+    catch (error) {
+      setTimeout(() => {
+        console.log('Default account could not be set. Retrying')
+        initialize()
+      }, 20000)
+    }
+  }
 
-    function initialize() {
+
+  function startws (_METAScenario, coinbasepwd) {
+    console.log('!!!!! Started Websocket')
+    const wsServer = new WebSocketServer({port: 20001})
+    wsServer.on('connection', (socket) => {
+      socket.on('message', (data) => {
         try {
-            var coinbasepwd = fs.readFileSync("/root/files/coinbasepwd", "utf8")
-            coinbasepwd = coinbasepwd.replace("\n", "")
-            let METAScenario = provider.eth.contract(abi).at(address);
-            provider.eth.defaultAccount = provider.eth.accounts[0];
-            let ip_address = {ipAddress: ip.address().toString()}
-            request({
-                url: "http://xain_contract_deployer:60000",
-                method: "POST",
-                json: true,
-                body: ip_address
-            }, function (error, response, body) {
-                try {
-                    console.log(error)
-                }
-                catch (error) {
-
-                }
-                console.log('connected')
-
-            });
-            startws(METAScenario, coinbasepwd)
+          console.log(data)
+          console.log('!!!!!!!!!!!!!Doing transaction')
+          provider.miner.stop()
+          provider.personal.unlockAccount(provider.eth.accounts[0], coinbasepwd)
+          const output = _METAScenario
+            .transfer('0x007ccffb7916f37f7aeef05e8096ecfbe55afc2f', 1, data)
+          provider.miner.start()
+          console.log(output)
         }
-        catch
-            (error) {
-            setTimeout(function () {
-                console.log("Default account could not be set. Retrying")
-                initialize()
-            }, 20000)
+        catch (error) {
+          console.log('!!!!!!!!!!!!!!!!!Transaction failed!!!!!!!!!')
+          console.log(error)
         }
-    }
+      })
+      socket.onerror = function (error) {
+        console.error(error)
+        setTimeout(() => {
+          socket.close()
+          startws()
+        }, 10000)
+      }
+    })
+  }
 
-
-    function startws(_METAScenario, coinbasepwd) {
-        console.log('!!!!! Started Websocket')
-        let wsServer = new WebSocketServer({port: 20001})
-        wsServer.on('connection', function connection(socket) {
-            socket.on('message', function incoming(data) {
-                try {
-                    console.log(data)
-                    console.log('!!!!!!!!!!!!!Doing transaction')
-                    provider.miner.stop()
-                    provider.personal.unlockAccount(provider.eth.accounts[0], coinbasepwd)
-                    let output = _METAScenario.transfer('0x007ccffb7916f37f7aeef05e8096ecfbe55afc2f', 1, data)
-                    provider.miner.start()
-                    console.log(output)
-                } catch (error) {
-                    console.log("!!!!!!!!!!!!!!!!!Transaction failed!!!!!!!!!")
-                    console.log(error)
-                }
-            })
-            socket.onerror = function (error) {
-                setTimeout(function () {
-                    socket.close()
-                    startws()
-                }, 10000)
-            }
-        })
-    }
-
-    initialize()
+  initialize()
 }
 
--- files/METAScenario/scripts/startSimulation.js
+++ files/METAScenario/scripts/startSimulation.js
@@ -1,78 +1,80 @@
-const WebSocket = require('ws');
-const execa = require("execa")
+const WebSocket = require('ws')
 const Web3 = require('web3')
 const web3Admin = require('web3admin')
 const fs = require('fs')
 const requiredBalance = 9999999999
-var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"students","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"},{"indexed":false,"name":"_data","type":"bytes"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"myBalance","outputs":[{"name":"myBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"generate","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"printAddress","outputs":[{"name":"self","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}]')
-var provider = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+const abi = require('../contractAbi.json')
+const provider = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
 web3Admin.extend(provider)
 
-function waitForContractAddress() {
-  var ws
-    ws = new WebSocket('ws://xain_contract_deployer:40000')
-     ws.on('message', function incoming(address) {
-         console.log("-------------------------Address-------------")
-         console.log(address)
-         startSimulation(address)
+function waitForContractAddress () {
+  const ws = new WebSocket('ws://xain_contract_deployer:40000')
+  ws.on('message', (address) => {
+    console.log('-------------------------Address-------------')
+    console.log(address)
+    startSimulation(address)
 
-     })
-  ws.onerror=function(event) {
-          console.log("Contract address WebSocket not reachable")
-          ws.close()
+  })
+  ws.onerror = function () {
+    console.log('Contract address WebSocket not reachable')
+    ws.close()
   }
 
-  ws.onclose=function(event){
-      setTimeout(function () {
-          waitForContractAddress()
-        }, 10000)
+  ws.onclose = function () {
+    setTimeout(() => {
+      waitForContractAddress()
+    }, 10000)
   }
 
 }
 
-function startSimulation(address) {
-    try {
-        if ((provider.eth.getBalance(provider.eth.accounts[0]).toString(10)) > requiredBalance) {
-            generateCoins(address)
-            setTimeout(function () {
-                require("./runTransactionSlave")(address)
-            }, 10000)
-        }
-        else {
-            setTimeout(function () {
-                startSimulation(address)
-            }, 10000)
-        }
-    } catch (error) {
-        setTimeout(function () {
-            startSimulation(address)
-        }, 10000)
+function startSimulation (address) {
+  try {
+    if (provider.eth.getBalance(provider.eth.accounts[0])
+      .toString(10) > requiredBalance) {
+      generateCoins(address)
+      setTimeout(() => {
+        require('./runTransactionSlave')(address)
+      }, 10000)
     }
+    else {
+      setTimeout(() => {
+        startSimulation(address)
+      }, 10000)
+    }
+  }
+  catch (error) {
+    setTimeout(() => {
+      startSimulation(address)
+    }, 10000)
+  }
 }
 
 
-function generateCoins(address) {
-        try {
-            const account = provider.eth.accounts[0]
-            var coinbasepwd = fs.readFileSync("/root/files/coinbasepwd", "utf8")
-            coinbasepwd = coinbasepwd.replace("\n", "")
-            var METAScenario = provider.eth.contract(abi).at(address)
-            provider.eth.defaultAccount = account
-            provider.miner.stop()
-            provider.personal.unlockAccount(account, coinbasepwd)
-            METAScenario.generate(requiredBalance)
-            provider.miner.start()
-            setTimeout(function () {
-                generateCoins(address)
-            }, 1000000)
-        } catch(error) {
-            provider.miner.start()
-            console.log("An error occured during generating coins")
-            console.log(error)
-            setTimeout(function () {
-                generateCoins(address)
-            }, 30000)
-        }
+function generateCoins (address) {
+  try {
+    const account = provider.eth.accounts[0]
+    let coinbasepwd = fs.readFileSync('/root/files/coinbasepwd', 'utf8')
+    coinbasepwd = coinbasepwd.replace('\n', '')
+    const METAScenario = provider.eth.contract(abi)
+      .at(address)
+    provider.eth.defaultAccount = account
+    provider.miner.stop()
+    provider.personal.unlockAccount(account, coinbasepwd)
+    METAScenario.generate(requiredBalance)
+    provider.miner.start()
+    setTimeout(() => {
+      generateCoins(address)
+    }, 1000000)
+  }
+  catch (error) {
+    provider.miner.start()
+    console.log('An error occured during generating coins')
+    console.log(error)
+    setTimeout(() => {
+      generateCoins(address)
+    }, 30000)
+  }
 
 
 }
